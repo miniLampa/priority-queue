3664e0db52bb5fc2cf375f0049e345a880a0d2d6
Natalia Litvinok <minilampa@gmail.com>
2019-03-24 23:59:23 +0300

    feat: add heap implementation

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,14 +2,45 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+    const node = new Node(data, priority);
+    this.insertNode(node);
+    this.shiftNodeUp(node);
 	}
 
-	pop() {
+	insertNode(node) {
+		if (!this.root) {
+			this.root = node;
+			this.parentNodes.push(node);
+			return;
+		}
+
+		const parentNode = this.parentNodes[0];
+
+		parentNode.appendChild(node);
+		this.parentNodes.push(node);
+
+		if (parentNode.left && parentNode.right) {
+			this.parentNodes.shift();
+		}
+  }
+
+  shiftNodeUp(node) {
+		if (node.parent) {	
+			if (node.priority > node.parent.priority) {
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			}
+		} else {
+			this.root = node;
+		}
+	}
+  
+  pop() {
 		
 	}
 
@@ -22,27 +53,26 @@ class MaxHeap {
 	}
 
 	size() {
-		
+		return this.parentNodes.length;
 	}
 
 	isEmpty() {
-		
+		return !this.root;
 	}
 
 	clear() {
 		
 	}
 
-	insertNode(node) {
-		
-	}
-
-	shiftNodeUp(node) {
-		
-	}
-
 	shiftNodeDown(node) {
-		
+		if (node.parent) {	
+			if (node.priority < node.parent.priority) {
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			}
+		} else {
+			this.root = node;
+		}
 	}
 }
 
--- src/node.js
+++ src/node.js
@@ -1,22 +1,82 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
+		if (this.left && this.right) {
+			return;
+		}
 
+		node.parent = this;
+
+		if (this.left) {
+			this.right = node;
+		} else {
+			this.left = node;
+		}
 	}
 
 	removeChild(node) {
-
+		if (this.left === node) {
+			node.parent = null;
+			this.left = null;
+		} else if (this.right === node) {
+			node.parent = null;
+			this.right = null;
+		} else {
+			throw new Error('Unassigned node');
+		}
 	}
 
 	remove() {
-
+		if (this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if (!this.parent) {
+			return;
+		}
+
+		const left = this.left;
+		const right = this.right;
+		const parent = this.parent;
+		const extra = this.parent.parent;
+
+		if (extra) {
+			this.parent = extra;
+			if (extra.left === parent) {
+				extra.left = this;
+			} else {
+				extra.right = this;
+			}
+		} else {
+			this.parent = null;
+		}
+
+		if (parent.left === this) {
+			this.left = parent;
+			this.right = parent.right;
+			if (this.right) {
+				this.parent = this;
+			}
+		} else {
+			this.right = parent;
+			this.left = parent.left;
+			if (this.left) {
+				this.left.parent = this;
+			}
+		}
+
+		parent.parent = this;
+		parent.left = left;
+		parent.right = right;
 	}
 }
 
